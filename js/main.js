86365a42abdf9b7e704d06c4d6eac56f159d51d9
SamSepiool <simone.severini@outlook.it>
2021-09-28 00:34:25 +0200

    add style

--- js/main.js
+++ js/main.js
@@ -14,16 +14,18 @@ const app = new Vue ({
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) =>{
                 this.randomEmails.push(response.data.response);
+                // (this.emailCount >= 10) ? this.emailCount++ : null;
             })
+            
+           
         }
-
     },
 
     mounted: function(){
         for(let i = 0; i < this.emailCount; i++){
-            this.newMail()
+            this.newMail();
         }
         console.log(this.randomEmails)
-    }
-    
+    },
 })
+
